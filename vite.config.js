7d733aaa4c0c5dda907e09e77e647e1d3f76253b
6510615294 <6510615294@student.tu.ac.th>
2026-02-12 02:28:26 +0700

    4

--- vite.config.js
+++ vite.config.js
@@ -4,5 +4,5 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: '/',  // Use '/' for user.github.io repos, or '/repo-name/' for project repos
+  base: 'https://6510615294.github.io',  // Use '/' for user.github.io repos, or '/repo-name/' for project repos
 })
